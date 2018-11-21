90031c7f1c408779051e73c207b62d2b8f5c5bf2
Nicolas Kijak <nkijak@gmail.com>
2018-11-21 15:21:48 +0000

    Getting table list from cte

--- sqlparsing/ast_parser.py
+++ sqlparsing/ast_parser.py
@@ -9,40 +9,72 @@ class AST(object):
 
 class ASTParser(Parser):
     def __init__(self):
-        self.ast = None
+        self._asts = [] #use as stack
         self.in_select = False
         self.in_from = False
+        self.in_with = False
+        self.sub_asts = []
+
+    @property
+    def asts(self):
+      return self.sub_asts
 
     def _visit_Statement(self, node):
-        self.ast = AST()
+        self._asts.append(AST())
+        print('visiting statement')
         for token in node.tokens:
             self._visit(token)
-        return self
+
+        self.sub_asts += self._asts
+        self._asts = []
+        return self.asts
 
     def _visit_Operation(self, node):
+        print('visiting operation')
         pass
 
     def _visit_Token(self, node):
-        if node.ttype == tokens.Keyword.DML and node.value.lower == 'select':
+        print('visiting token {} {}'.format(node.ttype, node.value.lower()))
+        if node.ttype == tokens.Keyword.DML and node.value.lower() == 'select':
             self.in_select = True
-        if node.ttype == tokens.Keyword and node.value.lower == 'from':
+        elif node.ttype == tokens.Keyword.CTE and node.value.lower() == 'with':
+            self.in_with = True
+        elif node.ttype == tokens.Keyword and node.value.lower() == 'from':
             self.in_from = True
         pass
 
     def _visit_IdentifierList(self, node):
+        print('visiting id list')
         for id in node.tokens:
             self._visit(id)
 
-    def _visit_Identifier(self, node):
-        names = [t.value for t in node.tokens]
         if self.in_select:
-            self.select = self.select + names
             self.in_select = False
-        if self.in_from:
-            self.table = self.table + names
+
+
+    def _visit_Identifier(self, node):
+        print('visiting id -- in_select?{} in_from?{} in_with?{}'.format(self.in_select, self.in_from, self.in_with))
+        print('id name={} alias={}'.format(node.get_name(), node.get_alias()))
+        names = [t.value for t in node.tokens]
+        ast = self._asts[-1]
+        if self.in_select:
+            ast.select = ast.select + names
+        elif self.in_from:
+            ast.table = ast.table + names
             self.in_from = False
+        elif self.in_with:
+            ast.table.append(node.get_name())
+            self._asts.append(AST())
+            for t in node.tokens:
+              self._visit(t)
         else:
             print('found ids {} but not sure what for'.format(names))
 
+    def _visit_Parenthesis(self, node):
+        print('visiting paren')
+        for id in node.tokens:
+            self._visit(id)
+        self.sub_asts.append(self._asts.pop())
+        
 
 
--- tests/ast_test.py
+++ tests/ast_test.py
@@ -1,9 +1,17 @@
 import pytest
 from sqlparsing.ast_parser import ASTParser
 
-def test_basic():
+def test_basic_tables():
     with open('tests/resources/basic.sql') as input:
         actual = ASTParser().parse(input)
 
-    assert actual[0].ast.table == ['a_table']
+    assert actual[0][0].table == ['a_table']
 
+def test_easy_cte_tables():
+    with open('tests/resources/easy_cte.sql') as input:
+        actual = ASTParser().parse(input)
+
+    actual_tables = []
+    for t in [a.table for a in actual[0]]:
+        actual_tables = actual_tables + t
+    assert actual_tables == ['sub_table', 'cte_table']
